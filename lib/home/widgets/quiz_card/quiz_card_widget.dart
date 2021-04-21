07f1dce47641d752713f8b15520737f923d241d8
Wender Lucas <wenderlucas.souza@hotmail.com>
2021-04-21 00:01:36 -0300

    challenge page and quiz card

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -36,11 +37,7 @@ class QuizCardWidget extends StatelessWidget {
                 ),
                 Expanded(
                   flex: 3,
-                  child: LinearProgressIndicator(
-                      value: 0.3,
-                      backgroundColor: AppColors.chartSecondary,
-                      valueColor: AlwaysStoppedAnimation<Color>(
-                          AppColors.chartPrimary)),
+                  child: ProgressIndicatorWidget(value: 0.3),
                 )
               ],
             ),
