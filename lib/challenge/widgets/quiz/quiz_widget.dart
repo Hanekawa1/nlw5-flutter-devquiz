41cc6efb478a20a7236e2a9aee7d641eb4717375
Wender Lucas <wenderlucas.souza@hotmail.com>
2021-04-24 13:18:12 -0300

    Navigation and challengePage

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,10 +1,26 @@
 import 'package:DevQuiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/models/answer_model.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
-  final String title;
-  const QuizWidget({Key? key, required this.title}) : super(key: key);
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final VoidCallback onChange;
+  const QuizWidget({
+    Key? key,
+    required this.question,
+    required this.onChange,
+  }) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int? indexSelected = -1;
+
+  AnswerModel answers(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
@@ -12,31 +28,26 @@ class QuizWidget extends StatelessWidget {
       child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
             child: Text(
-              title,
+              widget.question.title,
               style: AppTextStyles.heading,
             ),
           ),
           SizedBox(height: 24),
-          AnswerWidget(
-              isSelected: true,
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente."),
-          AnswerWidget(
-              isSelected: true,
-              isRight: true,
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente."),
-          AnswerWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente."),
-          AnswerWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente."),
-          AnswerWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente.")
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              answer: answers(i),
+              disabled: indexSelected != -1,
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+                setState(() {});
+                Future.delayed(
+                  Duration(seconds: 1),
+                ).then((value) => widget.onChange());
+              },
+            ),
         ],
       ),
     );
